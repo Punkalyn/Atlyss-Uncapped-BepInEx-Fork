9710a3f114efb3d2c88611eb1b019118942f99aa
ButteredLilly <BrandonBarrisoff@outlook.com>
2024-11-24 08:58:35 -0800

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using Uncapped_Players_And_Parties;
+
+
+[assembly: MelonInfo(typeof(Uncapper), "Lilly's Uncapped Players", "1.0.0", "Lilly")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Uncapped Players And Parties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Uncapped Players And Parties")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("18dd6f33-0186-4af7-b358-4e1c48bff4ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Uncapper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+using static MelonLoader.MelonLogger;
+
+namespace Uncapped_Players_And_Parties
+{
+    public class Uncapper : MelonMod
+    {
+        PartyObjectBehavior partyPre = null;
+        public override void OnLateInitializeMelon()
+        {
+            uncapParties();
+        }
+
+        void uncapParties()
+        {
+            try
+            {
+                if (partyPre == null)
+                {
+                    foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+                    {
+                        if (go.name == "_entity_partyObject")
+                        {
+                            partyPre = go.GetComponent<PartyObjectBehavior>();
+                            //MelonLogger.Msg(go.name);
+                            //MelonLogger.Msg(partyPre.Network_maxPartyLimit);
+                            partyPre.Network_maxPartyLimit = 1000;
+                            //MelonLogger.Msg(partyPre.Network_maxPartyLimit);
+                            //MelonLogger.Msg(partyPre._maxPartyLimit);
+                            partyPre._maxPartyLimit = 1000;
+                            //MelonLogger.Msg(partyPre._maxPartyLimit);
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Msg(e);
+            }
+        }
+
+        [HarmonyPatch(typeof(AtlyssNetworkManager), "OnStartServer")]
+        public static class uncapPlayers
+        {
+            private static void Prefix(ref AtlyssNetworkManager __instance)
+            {
+                try
+                {
+                    //MelonLogger.Msg(__instance.maxConnections);
+                    __instance.maxConnections = 500;
+                    //MelonLogger.Msg(__instance.maxConnections);
+                }
+                catch (Exception e)
+                {
+                    MelonLogger.Msg(e);
+                }
+            }
+        }
+    }
+}
